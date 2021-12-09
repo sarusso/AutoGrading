5a9db1d53e2ced69c8ae17b093793e3e042bde18
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-12-09 16:43:51 +0100

    Added stub grading example.

--- services/webapp/code/app.py
+++ services/webapp/code/app.py
@@ -1,6 +1,8 @@
-import logging
-from flask import Flask, render_template, request
 import uuid
+import logging
+
+from flask import Flask, render_template, request
+from utils import os_shell
 
 # Set up logging
 logging.basicConfig(format="[%(levelname)s] %(asctime)s %(message)s", level=logging.DEBUG)
@@ -20,6 +22,31 @@ def upload_file():
         f = request.files["file"]
         f.save("tmp/" + str(uuid.uuid4()) + ".py")
         return render_template("upload.html")
+
+@app.route("/grade/", methods=["GET"])
+def grade():
+    
+    # Initialize page data
+    data = {}
+    
+    # Create the docker run command.
+    # TODO: let's start by just executing the Python script uploaded by the user
+    command = 'sudo docker run hello-world'
+    
+    # Log the shell command going to be execute
+    logger.debug('Shell executing command: "%s"', command)
+    
+    # Execute the command and capture the output
+    out = os_shell(command, capture=True)
+    
+    # Log the output
+    logger.debug('Shell output: "%s"', out)
+    
+    # Set the output in the page data
+    data['shell_out'] = out
+
+    # Render
+    return render_template("grade.html", data=data)
 
 
 if __name__ == "__main__":

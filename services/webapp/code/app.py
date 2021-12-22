4165859e502d4884749cee59b2dc1eff9813e6c4
federicopigozzi <pigozzife@gmail.com>
2021-12-22 11:45:07 +0100

    grade receives uuid and exam name

--- services/webapp/code/app.py
+++ services/webapp/code/app.py
@@ -18,53 +18,54 @@ def menu():
     return render_template("menu.html")
 
 
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
+@app.route("/upload", methods=["POST"])
+def upload_file_and_exam():
+
+    f = request.files["file"]
+    exam_file = "test_" + request.form["esami"].replace(" ", "_") + ".py"
+    uuid_str = str(uuid.uuid4())
         
-        f = request.files["file"]
-        uuid_str = str(uuid.uuid4())
-        
-        # Save user file with unique id
-        destination_folder = "/shared/{}".format(uuid_str)
-        destination_file = destination_folder + "/esame.py"
-        try:
-            os.makedirs(destination_folder)
-        except PermissionError:
-            os_shell("sudo chmod 777 /shared")
-            os.makedirs(destination_folder)
+    # Save user file with unique id
+    destination_folder = "/shared/{}".format(uuid_str)
+    destination_file = destination_folder + "/esame.py"
+    try:
+        os.makedirs(destination_folder)
+    except PermissionError:
+        os_shell("sudo chmod 777 /shared")
+        os.makedirs(destination_folder)
             
-        f.save(destination_file)
-        logger.debug("Saved file to '%s'", destination_file)
-        
-        # Render
-        return render_template("upload.html")
+    f.save(destination_file)
+    logger.debug("Saved file to '%s'", destination_file)
+
+    # Save exam file in the same dir
+    source_exam_file = "/opt/webapp/code/" + exam_file
+
+    os_shell(" ".join(["cp", source_exam_file, destination_folder]))
+    logger.debug("Copied file to '%s'", source_exam_file)
+
+    return grade(uuid_str, exam_file)
 
 
-@app.route("/upload", methods=["GET", "POST"])
-def grade():
-    if request.method == "POST":
-        # Initialize page data
-        data = {}
+def grade(user_id, exam):
+    # Initialize page data
+    data = {}
+    # Create the docker run command.
+    command = "sudo docker run -v /tmp/autograding_shared/:/shared cd /shared/" + user_id + "/ && python:3.8 -m unittest discover"
     
-        # Create the docker run command.
-        # TODO: let's start by just executing the Python script uploaded by the user
-        command = "sudo docker run -v /tmp/autograding_shared/:/shared python:3.8 ls -alh /shared"
+    # Log the shell command going to be execute
+    logger.debug("Shell executing command: \"%s\"", command)
     
-        # Log the shell command going to be execute
-        logger.debug("Shell executing command: \"%s\"", command)
+    # Execute the command and capture the output
+    out = os_shell(command, capture=True)
     
-        # Execute the command and capture the output
-        out = os_shell(command, capture=True)
+    # Log the output
+    logger.debug("Shell output: \"%s\"", out)
     
-        # Log the output
-        logger.debug("Shell output: \"%s\"", out)
-    
-        # Set the output in the page data
-        data["shell_out"] = out
+    # Set the output in the page data
+    data["shell_out"] = out
 
-        # Render
-        return render_template("grade.html", data=data)
+    # Render
+    return render_template("grade.html", data=data)
 
 
 if __name__ == "__main__":
--- services/webapp/code/tester.py
+++
@@ -1,2 +0,0 @@
-if __name__ == "__main_":
-    print("0")
